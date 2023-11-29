82c26b725ad67d45664441ea857db4ff9856d361
moussa-nour <moussanour09@gmail.com>
2023-11-29 21:25:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+ export default class App extends Component {
+  constructor (props){
+    super(props)
+    this.state={
+      // settin state person
+      person:{
+      fullName:'Michael Jackson',
+      bio:' Michael Jackson, né le 29 août 1958  est un auteur-compositeur-interprète, danseur-chorégraphe et acteur américain',
+      imgSrc:'https://biografieonline.it/img/bio/m/Michael_Jackson.jpg',
+      profession:'Singer',
+      show:true},
+      intervalId: null,
+      timeSinceMount: 0,
+    }
+  }
+  // button state
+  show=()=>{
+    this.setState({show:!this.state.show})
+  }
+  // componentdidmount time
+  componentDidMount() {
+    this.setState({
+      intervalId: setInterval(() => {
+        this.setState((prevState) => ({
+          timeSinceMount: prevState.timeSinceMount + 1,
+        }));
+      }, 1000),
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { person,timeSinceMount } = this.state;
+    return (
+          <div className='App'>
+            <h1 style={{color:'#FFFFFF',marginTop:'100px',marginBottom:'20px'}}> </h1> {this.state.show &&
+            <Card style={{ width: '18rem',margin:'20px'}}>
+      <Card.Img  src={person.imgSrc}/>
+      <Card.Body>
+      <Card.Title>{person.fullName}</Card.Title>
+      <Card.Text>
+          {person.bio}  {person.profession}
+        </Card.Text>
+      </Card.Body>
+       </Card>}
+       <Button  onClick={this.show} style={{marginTop:'5px'}}>Show profile</Button>
+    <p style={{color:'#FFFFFF'}}>Time since mount: {timeSinceMount} seconds</p>
     </div>
-  );
-}
+    )
 
-export default App;
+            }}
